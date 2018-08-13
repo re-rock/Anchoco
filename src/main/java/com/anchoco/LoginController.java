a7cb910062628c8d5bfc6d121f25c07852662475
myuser <baysik734@gmail.com>
2018-08-13 20:05:25 +0900

    環境構築 MySQLの接続設定完了

--- src/main/java/com/anchoco/LoginController.java
+++ src/main/java/com/anchoco/LoginController.java
@@ -3,17 +3,21 @@ package com.anchoco;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
-	
-	@GetMapping("/login")
-    public Model messages(Model model) {
-		
-		String message = "勉強しましょう！";
-		
-        model.addAttribute("message", message);
 
+    @GetMapping("/login")
+    public Model messages(Model model) {
+        String message = "Hello World!";
+        model.addAttribute("message", message);
         return model;
+    }
+
+    @PostMapping("/top")
+    public Model initTop(Model model) {
+
+	return model;
     }
 }
