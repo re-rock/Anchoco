db9cebd4403b4aab10083278e21d5a624beffda6
myuser <baysik734@gmail.com>
2018-09-13 12:50:55 +0900

    ログイン画面の修正。nameとpasswordフォームの作成

--- src/main/java/com/anchoco/controller/LoginController.java
+++ src/main/java/com/anchoco/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.anchoco.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -15,9 +14,4 @@ public class LoginController {
         return model;
     }
 
-    @PostMapping("/top")
-    public Model initTop(Model model) {
-
-	return model;
-    }
 }
---
+++ src/main/java/com/anchoco/service/LoginService.java
@@ -0,0 +1,16 @@
+package com.anchoco.service;
+
+import org.springframework.stereotype.Service;
+
+import com.anchoco.form.LoginForm;
+
+@Service
+public class LoginService {
+
+    public boolean checkLoginUser(LoginForm loginForm) {
+
+        // TODO Login Check
+
+        return true;
+    }
+}
